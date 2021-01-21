fa9afb2a01ec6d91a7e6d78fc9ff9a1f2ca0ea9b
Deepa <deepa.vadakan@gmail.com>
2021-01-21 11:50:29 -0500

    breeds

--- app.py
+++ app.py
@@ -24,5 +24,13 @@ def home():
     # Return template and data
     return render_template("index.html")
 
+# route to find pet breeds
+@app.route("/breeds")
+def breeds():
+
+    return render_template("breeds.html")
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
