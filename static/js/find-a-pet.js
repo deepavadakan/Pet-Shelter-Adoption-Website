4e99dfc8f4266561e677f7fb36d9fd724f1ab63f
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2021-01-25 10:48:34 -0500

    working on find a pet map

--- static/js/find-a-pet.js
+++ static/js/find-a-pet.js
@@ -1,10 +1,12 @@
 // Creating map object
+
 var myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11
+    center: [45.52, -122.67],
+    zoom: 13
   });
   
-  // Adding tile layer to the map
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -13,14 +15,3 @@ var myMap = L.map("map", {
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
-  
-  //var usgsURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-  
-  //Request Data
-  //d3.json(usgsURL, function(data) {
-    //createFeatures(data.features);
-  //});
-  
-
-  
-   //createMap(findAPet);
