12e62efbea3e94a66ed9e32ff6f607747eb0571f
Deepa <deepa.vadakan@gmail.com>
2021-01-27 14:30:38 -0500

    added routes for dog, cat and none for find-a-pet

--- static/js/find-a-pet.js
+++ static/js/find-a-pet.js
@@ -1,49 +1,64 @@
-
-
 // Creating map object
-
 var myMap = L.map("map", {
-    center: [40.502754, -74.452221],
-    zoom: 13
-  });
-  console.log(myMap)
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  console.log(myMap)
-  console.log("find a pet")
+  center: [39.502754, -98.452221],
+  zoom: 5
+});
+console.log(myMap)
 
-  //Calling MongoDB Collection
+var markers;
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+console.log(myMap)
+console.log("find a pet")
+
+optionChanged("Dog");
+
+//Calling MongoDB Collection
 function optionChanged(dbSelected) {
-  var webApiPath = "";
+  var webApiPath = "/find-a-pet/" + dbSelected;
   console.log(dbSelected);
 
-  switch (dbSelected) {
-    case "mongodb":
-      webApiPath = "/mongodb-web-api";
-      break;
-    default:
-      console.log("An improper dropdown option has been selected.");
-      return;
-  }
+  // switch (dbSelected) {
+  //   case "mongodb":
+  //     webApiPath = "/mongodb-web-api";
+  //     break;
+  //   case "Dogs":
+  //     webApiPath = "/find-a-pet/dogs";
+  //     break;
+  //   case "Cats":
+  //       webApiPath = "/find-a-pet/cats";
+  //       break;
+  //   default:
+  //     console.log("An improper dropdown option has been selected.");
+  //     return;
+  // }
 
 
+  d3.json(webApiPath).then(function (webAPIData, err) {
+    if (err) { throw err };
+    if (!webAPIData) {
+        console.log("I wasn't able to get data from the Web API you selected.");
+        return;
+    }
 
-//Creating Markers
-for (var i = 0; i < dbSelected.length; i++) {
-    var id = dbSelected[i];
-    L.marker(id.name)
-      .bindPopup("<h1>" + id.address + "</h1> <hr> <h3>"+ id.id + "</h3> <hr> <h3>"+ id.Latitude +"</h3> <hr> <h3>"+ id.Longitude + "</h3>")
-      .addTo(myMap);
-  }
+    //console.log(webAPIData);
 
-  };
-    
+    //Creating Markers
+    for (var i = 0; i < webAPIData.length; i++) {
+      var id = webAPIData[i];
+      markers = L.marker([id.Latitude, id.Longitude])
+        .bindPopup(`<strong>Name: </strong>${id.name}<hr><strong>Breed: </strong>${id.breed}<br><strong>Link: </strong><a href='${id.url}' target='_blank'>Find me!</a>`)
+        .addTo(myMap);
+    }
+
+  });
+}
