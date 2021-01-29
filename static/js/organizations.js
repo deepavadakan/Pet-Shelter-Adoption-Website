412a14b4d3044936349c01334b051f00ab5a9e55
Deepa <deepa.vadakan@gmail.com>
2021-01-29 12:09:07 -0500

    set radius to number of pets

--- static/js/organizations.js
+++ static/js/organizations.js
@@ -2,6 +2,7 @@
 
 var data;
 
+// colors for circles
 var adultPetColor = '#6D98BA';
 var otherPetColor = '#EEC170';
 
@@ -9,16 +10,31 @@ var colorScale = {
   'Adult Dogs': '#EEC170',
 };
 
+// limits and circle radius lists
+var limits = [10, 20, 30, 40, 50, 60, 70, 80, 90, 200];
+var radius = [6, 8, 10, 12, 14, 16, 17, 18, 19, 20];
+
+// function to find the radius given the number of pets
+function getRadius(d) {
+  for (var i = 0; i < limits.length; i++) {
+    if (d < limits[i]) {
+      return radius[i];
+    }
+    else if (d > limits[limits.length - 1]) {
+      return 10 + radius[limits.length-1];
+    }
+  }
+}
+
 function addMarkers() {
   data.forEach(function (d) {
-    console.log(d);
     var marker = L.circleMarker([+d.Latitude, +d.Longitude])
-      .bindPopup(`<strong>Organization: </strong> <a href='${d.url_y}' target='_blank'>${d.name_y}</a><br><strong>Number of adoptable pets: </strong>${d["# of adoptable pets"]}<br><strong>Median pet age: </strong>${d.age}`);  ;
+      .bindPopup(`<strong>Organization: </strong> <a href='${d.url_y}' target='_blank'>${d.name_y}</a><hr style='margin:5px'><strong>Number of adoptable pets: </strong>${d["# of adoptable pets"]}<br><strong>Median pet age: </strong>${d.age}`);  ;
     var adult_dog = d.age === 'Adult';
     var color = colorScale[d.deviceControllerName] || '#6D98BA';
     if (adult_dog) {
       marker.setStyle({
-        radius: 8,
+        radius: getRadius(d["# of adoptable pets"]),
         fillColor: color,
         fillOpacity: 1,
         color: '#ddd',
@@ -26,7 +42,7 @@ function addMarkers() {
       });
     } else {
       marker.setStyle({
-        radius: 8,
+        radius: getRadius(d["# of adoptable pets"]),
         fillColor: otherPetColor,
         fillOpacity: 0.5,
         color: '#123',
@@ -43,10 +59,6 @@ var myMap = L.map("hexbin-map", {
   center: [39.502754, -98.452221],
   zoom: 5
 });
-console.log(myMap)
-
-// Create a marker cluster group
-// var markers = L.markerClusterGroup();
 
 // Adding a tile layer (the background map image) to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -57,7 +69,6 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   id: "mapbox/streets-v11",
   accessToken: MAPBOX_API_KEY
 }).addTo(myMap);
-console.log(myMap)
 
 // Reading from csv file and calling the Add Markers later on
 d3.csv('/static/data/hexbin_data.csv')
@@ -66,9 +77,7 @@ d3.csv('/static/data/hexbin_data.csv')
     addMarkers();
   });
 
-
 // Adding legend
-
 var legend = L.control({ position: 'topright' });
 
 legend.onAdd = function (myMap) {
@@ -76,7 +85,7 @@ legend.onAdd = function (myMap) {
   var div = L.DomUtil.create('div', 'info legend');
 
   // add title for legend
-  div.innerHTML = '<h4>Legend</h4>';
+  div.innerHTML = '<h5>Median Pet Age<br>per Organization</h5>';
 
   div.innerHTML += '<i style="background:' + adultPetColor + '"></i> Adult Pet<br>';
   div.innerHTML += '<i style="background:' + otherPetColor + '"></i> Other Pet<br>';
