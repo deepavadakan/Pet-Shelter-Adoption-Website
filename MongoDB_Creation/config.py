2688bf575a06a0e9b0db9b6f559487bf3d637cc2
Deepa Vadakan <69769279+deepavadakan@users.noreply.github.com>
2021-01-30 13:20:13 -0500

    Add files via upload

---
+++ MongoDB_Creation/config.py
@@ -0,0 +1,4 @@
+API_key = "YOUR API KEY FROM petfinder.com"
+API_secret = "YOUR SECRET FROM petfinder.com"
+mongo_conn = "mongodb://localhost:27017"
+#mongo_conn = 'mongodb+srv://<dbName>:<password>@cluster0.s0gp3.mongodb.net/rescue_angels_db?retryWrites=true&w=majority'
