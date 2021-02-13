7c898a732f0756b8fe184a8db9a6ab4fbc2a1ee5
Deepa <deepa.vadakan@gmail.com>
2021-02-13 16:32:11 -0500

    ReadME

--- MongoDB_Creation/config.py
+++ MongoDB_Creation/config.py
@@ -1,4 +1,4 @@
-API_key = "YOUR API KEY FROM petfinder.com"
-API_secret = "YOUR SECRET FROM petfinder.com"
+API_key = "PBeyYhc8LrmQ1VWEF4rMahj47htwaxrmuUoGzEtV0E6VQRU09r"
+API_secret = "Je5jUyTqEkThf14TJ7X6IpOJyMb0ONFekWOByuC6"
 mongo_conn = "mongodb://localhost:27017"
 #mongo_conn = 'mongodb+srv://<dbName>:<password>@cluster0.s0gp3.mongodb.net/rescue_angels_db?retryWrites=true&w=majority'
